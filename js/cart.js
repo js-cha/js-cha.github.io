ad4e722aa6918668a4b4f1d0c4b6dac235b2c764
Joseph Cha <josephcha4@gmail.com>
2016-07-06 22:51:57 +1000

    More work on shopping cart

--- js/cart.js
+++ js/cart.js
@@ -1,20 +1,24 @@
 (function(window){
   // TODO
-    //1 Add remove functionality
-    //2 Add hide and show cart functionality
-    //3 Add promo code
-    //4 Update subtotal number
+    //1 Add promo code
 
   // Declare cart object and variables
   var cart = {
     items: [],
-    total: 0
+    total: 0,
+    promos: ["BIGSALE"]
   }
+
   var add_to_cart_btn = document.querySelectorAll('.add-to-cart');
-  var cart_price = document.querySelector('.amount');
+  var shopping_cart = document.querySelector('.shopping-cart')
   var cart_summary = document.querySelector('.cart-summary');
-  var cart_qty;
+  var cart_total_price = document.querySelector('.amount');
+  var promo_field = document.querySelector('.promo-field');
+  var promo_btn = document.querySelector('.apply-promo');
+  var hide_show = document.querySelector('.open-cart');
   var cart_added_items;
+  var remove_item_btn;
+  var cart_qty;
 
   // Helper to check if item-to-be-added exists in the cart items array
   function itemExists(id){
@@ -27,6 +31,20 @@
     }
     return -1;
   };
+
+  // Remove item from shopping cart
+  function removeItem(event){
+    event.preventDefault();
+    var item_index = itemExists(this.parentElement.parentElement.parentElement.classList[1]);
+    var item_to_remove = this.parentElement.parentElement.parentElement;
+    cart.items.splice(item_index, 1);
+    item_to_remove.parentElement.removeChild(item_to_remove);
+    updateTotal();
+  }
+
+  hide_show.onclick = function(){
+    shopping_cart.classList.toggle('hidden');
+  }
 
   // Listen for cart product quantity when it becomes '0'
   function updateCartQty(id){
@@ -43,8 +61,13 @@
           cart.items.splice(itemIndex, 1);
           updateTotal();
         } else {
+          // Update total price
           cart.items[itemIndex].qty = this.value;
           updateTotal();
+          // Update item subtotal - item quantity * price
+          var cart_qty_subtotal = this.parentElement.nextSibling.firstChild;
+          cart.items[itemIndex].subtotal = cart.items[itemIndex].price * cart.items[itemIndex].qty;
+          cart_qty_subtotal.innerHTML = "$" + cart.items[itemIndex].subtotal;
         }
       }
     });
@@ -60,27 +83,32 @@
         productQty   = parseInt(product.querySelector('.unit-qty').value),
         productImg   = product.querySelector('.product-img').childNodes[1].src;
     pushItems(productId, productTitle, productPrice, productQty, productImg);
-    // this.classList.add("added");
-    // this.innerHTML = " Added"
-    // this.onclick = null;
   };
 
-  // Push item to cart object, items array
+  // Push item to cart object items array
   function pushItems(id, title, price, qty, img){
     if (cart.items.length <= 0){
-      cart.items.push({id: id, title: title, price: price, qty: qty});
+      cart.items.push({id: id, title: title, price: price, qty: qty, subtotal: price * qty});
     } else if (itemExists(id) >= 0){
-      var itemIndex = itemExists(id);
-      cart.items[itemIndex].qty += qty;
-      alert("This item has already been added");
+      alert("this item has already been added");
       return false;
     } else {
-      cart.items.push({id: id, title: title, price: price, qty: qty});
+      cart.items.push({id: id, title: title, price: price, qty: qty, subtotal: price * qty});
     }
     addMarkup(id, title, price, qty, img);
-    cart_summary.scrollIntoView();
     cart_qty = document.querySelectorAll('.cart-unit-qty');
     updateCartQty(id);
+
+    if(shopping_cart.classList.contains('hidden')){
+      shopping_cart.classList.toggle('hidden');
+    }
+    cart_summary.scrollIntoView();
+
+    // Attach onclick event handler for 'remove' buttons
+    remove_item_btn = document.querySelectorAll('.remove-product');
+    remove_item_btn.forEach(function(item, index, array){
+      item.onclick = removeItem;
+    });
   };
 
 
@@ -96,7 +124,7 @@
     var result = [],
         len = cart.items.length;
     if (len == 0){
-      cart_price.innerHTML = 0;
+      cart_total_price.innerHTML = 0;
       return false;
     }
     for (i=0; i < len; i++){
@@ -106,12 +134,20 @@
       return a + b;
     });
     cart.total = result;
-    cart_price.innerHTML = cart.total;
+    cart_total_price.innerHTML = cart.total;
   }
 
   // Attach event handler to all add-to-cart buttons
   add_to_cart_btn.forEach(function(item, index, array){
     item.onclick = productInfo;
   });
+
+  // promo_btn.onclick = function(){
+  //   for (i=0; i < cart.promos.length; i++){
+  //     if (promo_field.value == cart.promos[i]){
+  //       cart_total_price.innerHTML = cart.total * .95;
+  //     }
+  //   }
+  // }
 
 })(window);
