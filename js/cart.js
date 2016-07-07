3a033ed130f9a4b55d395717a8456d1bcdecb2b7
Joseph Cha <josephcha4@gmail.com>
2016-07-07 23:06:06 +1000

    more shopping... cart js stuff

--- js/cart.js
+++ js/cart.js
@@ -1,27 +1,69 @@
 (function(window){
-  // TODO
-    //1 Add promo code
-
-  // Declare cart object and variables
   var cart = {
     items: [],
     total: 0,
-    promos: ["BIGSALE"]
+    promos: {
+      BIGSALE: 0.25,
+      OVERW10: 0.10,
+      ORDER5:  0.05,
+    },
+    discounted: false,
+    promoCodeApplied: "",
+    discountPercent: 0
+  };
+
+  var addToCartBtns = document.querySelectorAll('.add-to-cart');
+  var cartSummary = document.querySelector('.cart-summary');
+  var cartTotalPrice = document.querySelector('.amount');
+  var codeApplied = document.querySelector('.code-added');
+  var deducted = document.querySelector('.discount-value');
+  var promoBtn = document.querySelector('.apply-promo');
+  var promoField = document.querySelector('.promo-field');
+  var shoppingCart = document.querySelector('.shopping-cart');
+  var toggleCartBtn = document.querySelector('.open-cart');
+
+  var cartAddedItems;
+  var cartQty;
+  var removeItemBtns;
+
+  addToCartBtns.forEach(function(item, index, array){
+    item.onclick = getProductInfo;
+  });
+
+  toggleCartBtn.onclick = function(){
+    shoppingCart.classList.toggle('hidden');
+  };
+
+  promoBtn.onclick = function(){
+    var promoCode = promoField.value;
+    if (promoCode == "OVERW10"){
+      // TODO: Add item specific discount
+    } else if (cart.promos.hasOwnProperty(promoCode) && cart.discounted == false){
+      var deductedValue = cart.promos[promoCode];
+      cart.discounted = true;
+      cart.promoCodeApplied = promoCode;
+      cart.discountPercent = deductedValue;
+      updateTotal();
+      codeApplied.innerHTML = "Promo code applied: " + "<strong>"+promoCode+"</strong>";
+      deducted.innerHTML = "Discount: " + deductedValue * 100 + "%" + " off total order";
+      codeApplied.classList.remove('hidden');
+      deducted.classList.remove('hidden');
+    } else if (promoCode.length <= 0){
+      return false;
+    } else {
+      alert("Only 1 promo code per transaction is allowed");
+    }
+  };
+
+  function resetPromoCode(){
+    promoField.value = "";
+    cart.discounted = false;
+    codeApplied.classList.add('hidden');
+    deducted.classList.add('hidden');
+    updateTotal();
   }
 
-  var add_to_cart_btn = document.querySelectorAll('.add-to-cart');
-  var shopping_cart = document.querySelector('.shopping-cart')
-  var cart_summary = document.querySelector('.cart-summary');
-  var cart_total_price = document.querySelector('.amount');
-  var promo_field = document.querySelector('.promo-field');
-  var promo_btn = document.querySelector('.apply-promo');
-  var hide_show = document.querySelector('.open-cart');
-  var cart_added_items;
-  var remove_item_btn;
-  var cart_qty;
-
-  // Helper to check if item-to-be-added exists in the cart items array
-  function itemExists(id){
+  function getItemIndex(id){
     var len = cart.items.length;
     for (i=0; i < len; i++){
       if (cart.items[i].id == id){
@@ -32,52 +74,46 @@
     return -1;
   };
 
-  // Remove item from shopping cart
   function removeItem(event){
     event.preventDefault();
-    var item_index = itemExists(this.parentElement.parentElement.parentElement.classList[1]);
-    var item_to_remove = this.parentElement.parentElement.parentElement;
-    cart.items.splice(item_index, 1);
-    item_to_remove.parentElement.removeChild(item_to_remove);
-    updateTotal();
-  }
+    var itemIndex = getItemIndex(this.parentElement.parentElement.parentElement.classList[1]);
+    var itemToRemove = this.parentElement.parentElement.parentElement;
+    cart.items.splice(itemIndex, 1);
+    itemToRemove.parentElement.removeChild(itemToRemove);
+    if (cart.items.length <= 0){
+      resetPromoCode();
+    } else {
+      updateTotal();
+    }
+  };
 
-  hide_show.onclick = function(){
-    shopping_cart.classList.toggle('hidden');
-  }
-
-  // Listen for cart product quantity when it becomes '0'
   function updateCartQty(id){
-    cart_qty.forEach(function(item, index, array){
+    cartQty.forEach(function(item, index, array){
       item.oninput = function(){
-        for (i=0; i < cart.items.length; i++){
-          if (cart.items[i].id == this.parentElement.parentElement.classList[1]){
-            var itemIndex = i;
-          }
-        }
+        var itemIndex = getItemIndex(this.parentElement.parentElement.classList[1]);
         if (this.value <= 0){
           var remove = this.parentElement.parentElement;
           remove.parentElement.removeChild(remove);
           cart.items.splice(itemIndex, 1);
           updateTotal();
         } else {
-          // Update total price
           cart.items[itemIndex].qty = this.value;
           updateTotal();
-          // Update item subtotal - item quantity * price
-          var cart_qty_subtotal = this.parentElement.nextSibling.firstChild;
+          var cartQtySubtotal = this.parentElement.nextSibling.firstChild;
           cart.items[itemIndex].subtotal = cart.items[itemIndex].price * cart.items[itemIndex].qty;
-          cart_qty_subtotal.innerHTML = "$" + cart.items[itemIndex].subtotal;
+          cartQtySubtotal.innerHTML = "$" + cart.items[itemIndex].subtotal;
         }
-      }
+        if (cart.items.length <= 0){
+          resetPromoCode();
+        }
+      };
     });
-  }
+  };
 
-  // Assign product information and then push information
-  function productInfo(event){
+  function getProductInfo(event){
     event.preventDefault();
     var productId = this.getAttribute('data-product'),
-        product   = document.getElementById(productId),
+        product = document.getElementById(productId),
         productTitle = product.querySelector('.product-title').innerHTML,
         productPrice = parseInt(product.querySelector('.unit-price').innerHTML),
         productQty   = parseInt(product.querySelector('.unit-qty').value),
@@ -85,46 +121,39 @@
     pushItems(productId, productTitle, productPrice, productQty, productImg);
   };
 
-  // Push item to cart object items array
   function pushItems(id, title, price, qty, img){
     if (cart.items.length <= 0){
       cart.items.push({id: id, title: title, price: price, qty: qty, subtotal: price * qty});
-    } else if (itemExists(id) >= 0){
+    } else if (getItemIndex(id) >= 0){
       alert("this item has already been added");
       return false;
     } else {
       cart.items.push({id: id, title: title, price: price, qty: qty, subtotal: price * qty});
     }
     addMarkup(id, title, price, qty, img);
-    cart_qty = document.querySelectorAll('.cart-unit-qty');
+    cartQty = document.querySelectorAll('.cart-unit-qty');
     updateCartQty(id);
-
-    if(shopping_cart.classList.contains('hidden')){
-      shopping_cart.classList.toggle('hidden');
+    if (shoppingCart.classList.contains('hidden')){
+      shoppingCart.classList.toggle('hidden');
     }
-    cart_summary.scrollIntoView();
-
-    // Attach onclick event handler for 'remove' buttons
-    remove_item_btn = document.querySelectorAll('.remove-product');
-    remove_item_btn.forEach(function(item, index, array){
+    cartSummary.scrollIntoView();
+    removeItemBtns = document.querySelectorAll('.remove-product');
+    removeItemBtns.forEach(function(item, index, array){
       item.onclick = removeItem;
     });
   };
 
-
-  // Add HTML markup to shopping cart
   function addMarkup(id, title, price, qty, img){
-    var cart_item = "<div class='cart-item "+id+"'><div class='cart-product'><div class='cart-product-summary'><div class='cart-product-img'><img src='"+img+"' alt='Legion' class='img-responsive'></div><div class='cart-product-info'>"+title+"</div><span class='cart-product-price'>$"+price+"</span><a href='#' class='remove-product'>Remove</a></div></div><div class='cart-qty'><input type='number' min='0' value="+qty+" class='unit-qty cart-unit-qty'></div><div class='cart-total'><div class='unit-qty-total'>$"+price * qty+"</div></div></div>";
-    cart_summary.insertAdjacentHTML("afterbegin", cart_item);
+    var cartItem = "<div class='cart-item "+id+"'><div class='cart-product'><div class='cart-product-summary'><div class='cart-product-img'><img src='"+img+"' alt='Legion' class='img-responsive'></div><div class='cart-product-info'>"+title+"</div><span class='cart-product-price'>$"+price+"</span><a href='#' class='remove-product'>Remove</a></div></div><div class='cart-qty'><input type='number' min='0' value="+qty+" class='unit-qty cart-unit-qty'></div><div class='cart-total'><div class='unit-qty-total'>$"+price * qty+"</div></div></div>";
+    cartSummary.insertAdjacentHTML("afterbegin", cartItem);
     updateTotal();
   };
 
-  // Update total price from cart object
-  function updateTotal(){
-    var result = [],
-        len = cart.items.length;
+  function updateTotal(code, value){
+    var result = [];
+    var len = cart.items.length;
     if (len == 0){
-      cart_total_price.innerHTML = 0;
+      cartTotalPrice.innerHTML = 0;
       return false;
     }
     for (i=0; i < len; i++){
@@ -133,21 +162,12 @@
     result = result.reduce(function(a, b){
       return a + b;
     });
-    cart.total = result;
-    cart_total_price.innerHTML = cart.total;
-  }
-
-  // Attach event handler to all add-to-cart buttons
-  add_to_cart_btn.forEach(function(item, index, array){
-    item.onclick = productInfo;
-  });
-
-  // promo_btn.onclick = function(){
-  //   for (i=0; i < cart.promos.length; i++){
-  //     if (promo_field.value == cart.promos[i]){
-  //       cart_total_price.innerHTML = cart.total * .95;
-  //     }
-  //   }
-  // }
-
+    if (cart.discounted == true){
+      cart.total = result - (result * cart.discountPercent);
+      cartTotalPrice.innerHTML = cart.total;
+    } else {
+      cart.total = result;
+      cartTotalPrice.innerHTML = cart.total;
+    }
+  };
 })(window);
