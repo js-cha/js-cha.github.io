a9a6ae533896e4e420a71400b1efaa2141e08f58
Joseph Cha <josephcha4@gmail.com>
2017-09-14 17:30:19 +1000

    use vanilla js

--- projects/innovative_component/javascript.js
+++ projects/innovative_component/javascript.js
@@ -1,89 +1,57 @@
-;(function($) {
+function PhotoBlog(options) {
+  this.element = document.getElementById('photo-blog-component');
+  this.options = options;
+  this.nodesArray = [];
+  this.init();
+}
 
-  var defaults = {
-    // set defaults here
-  };
+PhotoBlog.prototype.init =  function() {
+  this.processConfig();
+  this.render();
+};
 
-  function PhotoBlog(element, options) {
-    this.config = $.extend({}, defaults, options);
-    this.element = element;
-    this.element.data('photoblog', this);
-    this.nodesArray = [];
-    this.init();
-  }
+PhotoBlog.prototype.processConfig = function() {
+  var self = this;
+  this.options.forEach(function(value) {
+    self.createMarkup(value);
+  });
+};
 
-  PhotoBlog.prototype.init =  function() {
-    this.processConfig();
-    this.render();
-  };
+PhotoBlog.prototype.createMarkup = function(obj) {
+  var src = obj.link,
+      alt = obj.alt,
+      text = obj.text,
+      credit = obj.credit;
 
-  PhotoBlog.prototype.processConfig = function() {
-    var self = this;
-    $.each(this.config, function(index, value) {
-      self.createMarkup(value);
-    });
-  };
+  var section = document.createElement('section');
+      section.setAttribute('class', 'photo-blog__section');
 
-  PhotoBlog.prototype.createMarkup = function(obj) {
-    var src = obj.link,
-        alt = obj.alt,
-        text = obj.text,
-        credit = obj.credit;
+  var image = document.createElement('img');
+      image.setAttribute('class', 'photo-blog__media');
+      image.setAttribute('src', src);
+      image.setAttribute('alt', alt);
 
-    var section = document.createElement('section');
-        section.setAttribute('class', 'photo-blog__section');
+  var description = document.createElement('p');
+      description.setAttribute('class', 'photo-blog__text');
+      description.innerText = text;
 
-    var image = document.createElement('img');
-        image.setAttribute('class', 'photo-blog__media');
-        image.setAttribute('src', src);
-        image.setAttribute('alt', alt);
+  var author = document.createElement('small');
+      author.setAttribute('class', 'photo-blog__media-credit');
+      author.innerText = credit;
 
-    var description = document.createElement('p');
-        description.setAttribute('class', 'photo-blog__text');
-        description.innerText = text;
+  section.append(description);
+  section.append(image);
+  section.append(author);
 
-    var author = document.createElement('small');
-        author.setAttribute('class', 'photo-blog__media-credit');
-        author.innerText = credit;
+  this.nodesArray.push(section);
+};
 
-    section.append(description);
-    section.append(image);
-    section.append(author);
+PhotoBlog.prototype.render = function() {
+  var docFrag = document.createDocumentFragment();
 
-    this.nodesArray.push(section);
-  };
+  this.nodesArray.forEach(function(value) {
+    docFrag.append(value);
+  });
 
-  PhotoBlog.prototype.render = function() {
-    var docFrag = document.createDocumentFragment();
-
-    this.nodesArray.forEach(function(value) {
-      docFrag.append(value);
-    });
-
-    this.element.append(docFrag);
-  }
-
-  PhotoBlog.prototype.options = function(option, val) {
-    this.config[option] = val;
-  };
-
-  $.fn.photoblog = function (options) {
-    var args = $.makeArray(arguments);
-    var after = args.slice(1);
-    this.each(function () {
-      var $el = $(this);
-      var instance;
-      if (instance = $el.data('photoblog')) {
-        if (typeof(options) === 'string' && typeof(instance[options]) == 'function') {
-          instance[options].apply(instance, after);
-        } else {
-          console.warn(' already initialized on', this, 'and', options, 'is not a valid instance method');
-        }
-        return;
-      }
-      new PhotoBlog($el, options);
-    });
-    return this;
-  };
-
-})(jQuery);
+  this.element.append(docFrag);
+}
