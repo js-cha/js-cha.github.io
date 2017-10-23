5daf5213dacd56398c9fdfef72ed635c9a360ddb
Joseph Cha <josephcha4@gmail.com>
2017-10-24 01:46:08 +1100

    update component

--- projects/innovative_component/javascript.js
+++ projects/innovative_component/javascript.js
@@ -6,102 +6,113 @@ function PhotoBlog(title, options) {
   this.init();
 }
 
-function createSection() {
-  var section = document.createElement('section');
-  section.setAttribute('class', 'photo-blog__section');
+function setMultiAttrs(element, obj) {
+  for (var prop in obj) {
+    element.setAttribute(prop, obj[prop]);
+  }
+}
 
-  return section;
+function generateHTML(tag, klass, inner) {
+  var element = document.createElement(tag);
+  element.setAttribute('class', klass);
+  if (typeof inner !== undefined) {
+    element.innerHTML = inner || '';
+  }
+  return element;
+}
+
+function createImageCarousel(obj) {
+  var docFrag         = document.createDocumentFragment(),
+      mainImage       = document.createElement('img'),
+      cycleButton     = document.createElement('button'),
+      imageContainer  = generateHTML('div', 'img-container'),
+      mediaCredit     = generateHTML('small', 'photo-blog__media-credit', obj.media.src[0].credit);
+      images          = obj.media.src,
+
+  setMultiAttrs(mainImage, {
+    class: 'photo-blog__media',
+    data_img_pos: '0',
+    src: images[0].src,
+    alt: images[0].alt
+  });
+
+  imageContainer.append(mainImage);
+  imageContainer.append(cycleButton);
+
+  cycleButton.innerText = 'Next';
+  cycleButton.addEventListener('click', function cycle() {
+    var siblingImage = this.previousSibling;
+    var currPos = Number(siblingImage.getAttribute('data_img_pos'));
+    if (currPos === images.length - 1 ) {
+      setMultiAttrs(siblingImage, {
+        data_img_pos: '0',
+        src: images[0].src,
+        alt: images[0].alt
+      })
+      mediaCredit.innerText = images[Number(0)].credit
+    } else {
+      setMultiAttrs(siblingImage, {
+        data_img_pos: Number(currPos + 1),
+        src: images[Number(currPos + 1)].src,
+        alt: images[Number(currPos + 1)].alt
+      })
+      mediaCredit.innerText = images[Number(currPos + 1)].credit
+    }
+  });
+
+  docFrag.appendChild(imageContainer);
+  docFrag.appendChild(mediaCredit);
+  return docFrag;
 }
 
 function createImageObject(obj) {
-  if (obj.media.src.constructor === Array) {
-    var docFrag = document.createDocumentFragment();
-    var imageContainer = document.createElement('div');
-    var mainImage = document.createElement('img');
-    var cycleButton = document.createElement('button');
-    var images = obj.media.src;
-    var mediaCredit = createMediaCredit(obj.media.src[0].credit);
-
-    mainImage.setAttribute('class', 'photo-blog__media');
-    mainImage.setAttribute('data-img-pos', '0');
-    mainImage.setAttribute('src', images[0].src);
-    mainImage.setAttribute('alt', images[0].alt);
-
-    imageContainer.append(mainImage);
-    imageContainer.append(cycleButton);
-    imageContainer.setAttribute('class', 'img-container');
-
-    cycleButton.innerText = 'Next';
-    cycleButton.addEventListener('click', function cycle() {
-      var siblingImage = this.previousSibling;
-      var currPos = Number(siblingImage.getAttribute('data-img-pos'));
-
-      if (currPos === images.length - 1 ) {
-        siblingImage.setAttribute('data-img-pos', '0');
-        siblingImage.setAttribute('src', images[Number(0)].src);
-        siblingImage.setAttribute('alt', images[Number(0)].alt);
-        mediaCredit.innerText = images[Number(0)].credit
-      } else {
-        siblingImage.setAttribute('data-img-pos', Number(currPos + 1));
-        siblingImage.setAttribute('src', images[Number(currPos + 1)].src);
-        siblingImage.setAttribute('alt', images[Number(currPos + 1)].alt);
-        mediaCredit.innerText = images[Number(currPos + 1)].credit
-      }
-    });
-
-    docFrag.appendChild(imageContainer);
-    docFrag.appendChild(mediaCredit);
-
-    return docFrag;
+  if (Array.isArray(obj.media.src)) {
+    var carousel = createImageCarousel(obj);
+    return carousel;
   } else {
-    var docFrag = document.createDocumentFragment();
-    var image = document.createElement('img');
-    var mediaCredit = createMediaCredit(obj.media.credit);
+    var docFrag     = document.createDocumentFragment(),
+        image       = document.createElement('img'),
+        mediaCredit = generateHTML('small', 'photo-blog__media-credit', obj.media.credit);
 
-    image.setAttribute('class', 'photo-blog__media');
-    image.setAttribute('src', obj.media.src);
-    image.setAttribute('alt', obj.media.alt);
+    setMultiAttrs(image, {
+      class: 'photo-blog__media',
+      src: obj.media.src,
+      alt: obj.media.alt
+    });
 
     docFrag.appendChild(image)
     docFrag.appendChild(mediaCredit)
-
     return docFrag;
   }
-
 }
 
 function createVideoObject(obj) {
-  var docFrag = document.createDocumentFragment();
+  var docFrag        = document.createDocumentFragment(),
+      poster         = document.createElement('img');
+      videoContainer = generateHTML('div', 'video-container'),
+      mediaCredit    = generateHTML('small', 'photo-blog__media-credit', obj.media.credit),
 
-  var mediaCredit = createMediaCredit(obj.media.credit);
-
-  var videoContainer = document.createElement('div');
-  videoContainer.setAttribute('class', 'video-container');
-  videoContainer.classList.add('play-btn');
-
-  var poster = document.createElement('img');
   poster.setAttribute('src', obj.media.poster);
-
+  videoContainer.classList.add('play-btn');
   videoContainer.append(poster);
 
   videoContainer.addEventListener('click', function lazyLoad(event) {
     var video = document.createElement('video');
-    video.setAttribute('width', '100%');
-    video.setAttribute('class', 'photo-blog__media');
-    video.setAttribute('src', obj.media.src);
-    video.setAttribute('poster', obj.media.poster);
-    video.setAttribute('controls', '');
-    video.setAttribute('autoplay', '');
-
+    setMultiAttrs(video, {
+      width: '100%',
+      class: 'photo-blog__media',
+      src: obj.media.src,
+      poster: obj.media.poster,
+      controls: '',
+      autoplay: '',
+    });
     this.innerHTML = '';
     this.append(video);
     this.classList.remove('play-btn');
     this.removeEventListener('click', lazyLoad);
   });
-
   docFrag.appendChild(videoContainer)
   docFrag.appendChild(mediaCredit);
-
   return docFrag;
 }
 
@@ -120,34 +131,6 @@ function createMediaObject(obj) {
   return media;
 }
 
-function createMediaCredit(credit) {
-  var author = document.createElement('small');
-  author.setAttribute('class', 'photo-blog__media-credit');
-  author.innerText = credit;
-
-  return author;
-}
-
-function createHeading(str) {
-  var heading = document.createElement('h3');
-  heading.setAttribute('class', 'photo-blog__heading');
-  heading.innerText = str;
-
-  return heading;
-}
-
-function createDescription(str) {
-  var description = document.createElement('p');
-  description.setAttribute('class', 'photo-blog__text');
-  description.innerText = str;
-
-  return description;
-}
-
-PhotoBlog.prototype.init =  function() {
-  this.processConfig();
-  this.render();
-};
 
 PhotoBlog.prototype.processConfig = function() {
   this.options.forEach(function(value) {
@@ -156,14 +139,14 @@ PhotoBlog.prototype.processConfig = function() {
 };
 
 PhotoBlog.prototype.createMarkup = function(obj) {
-  var media = createMediaObject(obj),
-      desc = createDescription(obj.text),
-      section = createSection();
+  var media   = createMediaObject(obj),
+      desc    = generateHTML('p', 'photo-blog__text', obj.text),
+      section = generateHTML('section', 'photo-blog__section');
 
   if (obj.heading) {
-    var heading = createHeading(obj.heading);
-    section.append(media);
+    var heading = generateHTML('h3', 'photo-blog__heading', obj.heading);
     section.append(heading);
+    section.append(media);
     section.append(desc);
   } else {
     section.append(media);
@@ -174,15 +157,83 @@ PhotoBlog.prototype.createMarkup = function(obj) {
 };
 
 PhotoBlog.prototype.render = function() {
-  var docFrag = document.createDocumentFragment();
-
+  var docFrag   = document.createDocumentFragment();
   var mainTitle = document.createElement('h1');
   mainTitle.innerText = this.title;
-
   this.nodesArray.forEach(function(value) {
     docFrag.append(value);
   });
-
   this.element.append(mainTitle);
   this.element.append(docFrag);
 }
+
+PhotoBlog.prototype.nav = function() {
+  var componentBCR = this.element.getBoundingClientRect();
+
+  window.addEventListener('load', function() {
+    var container = document.querySelector('.photo-blog-container'),
+        docFrag   = document.createDocumentFragment(),
+        nav       = document.createElement('div'),
+        sections  = document.querySelectorAll('.photo-blog__section'),
+        sectionCoords = [];
+
+    for (i = 0; i < sections.length; i++) {
+      var indicator = document.createElement('div');
+      indicator.classList.add('indicator');
+
+      sectionCoords.push({
+        start: sections[i].offsetTop,
+        end: sections[i].offsetTop + sections[i].offsetHeight - 1,
+      });
+
+      nav.classList.add('nav');
+      nav.append(indicator);
+    }
+
+    docFrag.append(nav);
+    container.insertBefore(docFrag, this.element);
+    nav.style.top = componentBCR.top + 'px';
+
+    var indicators = [].slice.call(document.querySelectorAll('.indicator'));
+
+    function indicatorClick() {
+      indicators.forEach(function(value, index) {
+        if (event.target == value) {
+          indicators[index].classList.add('active-section');
+          sections[index].scrollIntoView();
+        } else {
+          indicators[index].classList.remove('active-section');
+        }
+      });
+    }
+
+    function indicatorScroll() {
+      sectionCoords.forEach(function(value, index, arr) {
+        if (window.scrollY >= value.start && window.scrollY <= value.end) {
+          indicators[index].classList.add('active-section');
+        } else {
+          indicators[index].classList.remove('active-section');
+        }
+      });
+    }
+
+    function throttle(method, scope) {
+      clearTimeout(method._tId);
+      method._tId= setTimeout(function(){
+          method.call(scope);
+      }, 50);
+    }
+
+    nav.addEventListener('click', indicatorClick);
+
+    window.addEventListener('scroll', function() {
+      throttle(indicatorScroll, window);
+    });
+  });
+}
+
+PhotoBlog.prototype.init =  function() {
+  this.processConfig();
+  this.render();
+  this.nav();
+};
